b9283337722d78db68a877ac9a3f47a1f8b9e631
NeonMariia <95533543+NeonMariia@users.noreply.github.com>
2022-07-14 18:49:29 +0200

    Github Automation (#8)
    * adding unittests and requirements folder
    
    * Delete build/lib/googletranslate_neon_plugin directory
    
    * adding github automation
    
    * change from pull to pull_request
    
    * remove ref spec
    
    * licence headers, setup.py fixes
    
    * version fixes

--- version.py
+++ version.py
@@ -1,9 +1,6 @@
-# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
 # All trademark and other rights reserved by their respective owners
-# Copyright 2008-2022 Neongecko.com Inc.
-# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
-# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
-# BSD-3 License
+# Copyright 2008-2021 Neongecko.com Inc.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 # 1. Redistributions of source code must retain the above copyright notice,
@@ -26,40 +23,4 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
-from ovos_plugin_manager.templates.language import LanguageDetector,\
-    LanguageTranslator
-from google_tx import google_translator
-
-
-class GoogleTranslator(LanguageTranslator):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.translator = google_translator()
-        self.boost = False
-
-    def translate(self, text, target=None, source=None):
-        if self.boost and not source:
-            source = self.default_language
-        target = target or self.internal_language
-        if source:
-            tx = self.translator.translate(text, lang_src=source.split("-")[0],
-                                           lang_tgt=target.split("-")[0])
-        else:
-            tx = self.translator.translate(text, lang_tgt=target.split("-")[0])
-        return tx.strip()
-
-
-class GoogleDetector(LanguageDetector):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.translator = google_translator()
-
-    def detect(self, text):
-        tx = self.translator.detect(text)
-        return tx[0] or self.default_language
-
-    def detect_probs(self, text):
-        tx = self.translator.detect(text)
-        return {"lang_code": tx[0], "lang": tx[1]}
-
+__version__ = "0.1.0"
